75e437320c9f6c244c6656a90a42359a460bf2a6
Fernando R B de Tulio <fernandorbdetulio@Mac-mini-de-Fernando.local>
2019-01-25 21:47:27 -0200

    Ad2

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -142,7 +142,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 LOGIN_REDIRECT_URL = 'home'
 
