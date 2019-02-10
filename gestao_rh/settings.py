ec32ad00ea59e53d718de74a2c55713fde509c02
Fernando R B de Tulio <fernandorbdetulio@Mac-mini-de-Fernando.local>
2019-02-10 10:10:20 -0200

    Conf3

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -88,8 +88,6 @@ WSGI_APPLICATION = 'gestao_rh.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
@@ -107,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
