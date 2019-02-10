9dc7f13c3cf3ebdb9462e13f0a4de1620f29eb6e
Fernando R B de Tulio <fernandorbdetulio@Mac-mini-de-Fernando.local>
2019-02-10 10:08:59 -0200

    Conf3

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -86,12 +86,7 @@ WSGI_APPLICATION = 'gestao_rh.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 
 
@@ -149,4 +144,4 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
 
 
-#from .local_settings import *
+from .local_settings import *
